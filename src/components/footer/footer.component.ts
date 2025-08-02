fbf24e69bc1491f595074563adaecba9c8b2b969
poold3 <dallinpool@gmail.com>
2025-08-02 17:50:34 -0400

    Adding npm and GitHub links to footer.

--- src/components/footer/footer.component.ts
+++ src/components/footer/footer.component.ts
@@ -5,10 +5,12 @@ import footerTemplate from "./footer.component.html?raw";
 import footerStyles from "./footer.component.css?raw";
 
 export class FooterComponent extends Component {
-  appTitle = new Value(Constants.TITLE);
-  appSlogan = new Value(Constants.SLOGAN);
-  privacyURL = new Value(`https://${import.meta.env.VITE_CLIENT_URL}/privacy`);
-  supportEmail = new Value(Constants.SUPPORT_EMAIL);
+  private appTitle = new Value(`${Constants.TITLE} ${Constants.EMOJI_ICON}`);
+  private appSlogan = new Value(Constants.SLOGAN);
+  private npmURL = new Value(Constants.NPM_URL);
+  private srcURL = new Value(Constants.SOURCE_CODE_URL);
+  private docsURL = new Value(Constants.DOCS_URL);
+  private supportEmail = new Value(Constants.SUPPORT_EMAIL);
 
   constructor() {
     super({
@@ -30,9 +32,19 @@ export class FooterComponent extends Component {
       this.appSlogan.bindElementProperty(sloganParagraph, "innerText");
     }
 
-    const privacyLink = this.getChild<HTMLAnchorElement>("#privacy-link");
-    if (privacyLink) {
-      this.privacyURL.bindElementAttribute(privacyLink, "href");
+    const npmLink = this.getChild<HTMLAnchorElement>("#npm-link");
+    if (npmLink) {
+      this.npmURL.bindElementAttribute(npmLink, "href");
+    }
+
+    const srcLink = this.getChild<HTMLAnchorElement>("#source-link");
+    if (srcLink) {
+      this.srcURL.bindElementAttribute(srcLink, "href");
+    }
+
+    const docsLink = this.getChild<HTMLAnchorElement>("#documentation-link");
+    if (docsLink) {
+      this.docsURL.bindElementAttribute(docsLink, "href");
     }
 
     const contactLink = this.getChild<HTMLAnchorElement>("#contact-link");
