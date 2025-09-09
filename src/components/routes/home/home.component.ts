7b17ce51a3e7d3a012216d9a42f49c0c5795cc00
poold3 <dallinpool@gmail.com>
2025-09-08 22:13:10 -0400

    Fixing and planning routes.

--- src/components/routes/home/home.component.ts
+++ src/components/routes/home/home.component.ts
@@ -30,7 +30,7 @@ export class HomeComponent extends Component {
     const getStartedButton = this.getChild<HTMLButtonElement>("#get-started");
     if (getStartedButton) {
       getStartedButton.addEventListener("click", () => {
-        RouterService.route("/getting-started");
+        RouterService.route("/sapling/getting-started");
       });
     }
   }
--- src/config/router.ts
+++ src/config/router.ts
@@ -1,13 +1,18 @@
 import { RouterService } from "@craiglington/sapling";
 import { HomeComponent } from "../components/routes/home/home.component";
 import { GettingStartedComponent } from "../components/routes/sapling/getting-started/getting-started.component";
+import { ComponentComponent } from "../components/routes/sapling/component/component.component";
 
 export function initRouterService() {
   RouterService.init({
     routes: [
       {
-        path: new RegExp(/^\/getting-started$/),
+        path: new RegExp(/^\/sapling\/getting-started$/),
         component: GettingStartedComponent
+      },
+      {
+        path: new RegExp(/^\/sapling\/component$/),
+        component: ComponentComponent
       },
       {
         path: new RegExp(/^\/$/),
